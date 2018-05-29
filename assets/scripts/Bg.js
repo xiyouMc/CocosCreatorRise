cd08ea6a5775510fc9a99f8d0294b46ba43b064c
xiyouMc <xiyoumc.dev@gmail.com>
2018-05-29 10:05:44 +0800

    init code

---
+++ assets/scripts/Bg.js
@@ -0,0 +1,31 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // player 节点，用于获取主角弹跳的高度，和控制主角行动开关
+        ball: {
+            default: null,
+            type: cc.Node
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start() {
+
+    },
+
+    // update (dt) {},
+});
---
+++ assets/scripts/Luoti.js
@@ -0,0 +1,86 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad() {
+        // this.node.x;
+        // var luoti = cc.moveBy(2, cc.p(0, -100)).easing(cc.ease());
+        // // 添加一个回调函数，用于在动作结束时调用我们定义的其他方法
+        // var callback = cc.callFunc(this.playJumpSound, this);
+        // this.node.runAction(cc.sequence(luoti, callback))
+        cc.director.getPhysicsManager().enabled = true;
+
+        var manager = cc.director.getCollisionManager();
+        manager.enabled = true;
+        manager.enabledDebugDraw = true;
+        manager.enabledDrawBoundingBox = true;
+    },
+    playJumpSound: function () {
+        // 调用声音引擎播放声音
+        console.log('ffff')
+    },
+    start() {
+
+    },
+
+    // update (dt) {},
+
+    /**
+ * 当碰撞产生的时候调用
+ * @param  {Collider} other 产生碰撞的另一个碰撞组件
+ * @param  {Collider} self  产生碰撞的自身的碰撞组件
+ */
+    onCollisionEnter: function (other, self) {
+        console.log('on collision enter');
+
+        // 碰撞系统会计算出碰撞组件在世界坐标系下的相关的值，并放到 world 这个属性里面
+        var world = self.world;
+
+        // 碰撞组件的 aabb 碰撞框
+        var aabb = world.aabb;
+
+        // 上一次计算的碰撞组件的 aabb 碰撞框
+        var preAabb = world.preAabb;
+
+        // 碰撞框的世界矩阵
+        var t = world.transform;
+
+        // 以下属性为圆形碰撞组件特有属性
+        var r = world.radius;
+        var p = world.position;
+
+        // 以下属性为 矩形 和 多边形 碰撞组件特有属性
+        var ps = world.points;
+    },
+    /**
+     * 当碰撞产生后，碰撞结束前的情况下，每次计算碰撞结果后调用
+     * @param  {Collider} other 产生碰撞的另一个碰撞组件
+     * @param  {Collider} self  产生碰撞的自身的碰撞组件
+     */
+    onCollisionStay: function (other, self) {
+        console.log('on collision stay');
+    },
+    /**
+     * 当碰撞结束后调用
+     * @param  {Collider} other 产生碰撞的另一个碰撞组件
+     * @param  {Collider} self  产生碰撞的自身的碰撞组件
+     */
+    onCollisionExit: function (other, self) {
+        console.log('on collision exit');
+    }
+});
